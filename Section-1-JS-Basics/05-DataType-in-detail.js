c284793cf799876995bd57bff609e192471f8cb9
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-07-13 22:17:47 +0530

    Data Types in Detail

---
+++ Section-1-JS-Basics/05-DataType-in-detail.js
@@ -0,0 +1,34 @@
+/*
+==============TYPES================
+1. Primitives: String, Boolean, Number, null, undefined, Symbol
+
+2. Non-Premitive: Mostly Objects: array, functions
+*/
+
+
+/*
+=============PRIMITIVES==============
+
+*/
+
+//Numbers
+
+let balance = 1000;
+console.log(typeof balance);    //number
+console.log(balance.valueOf()); //1000
+
+
+
+
+/*
+================NON - PREMITIVES
+*/
+ let obj = {};
+ console.log(typeof obj); //object
+
+ const person = {
+    name : "Ankit",
+    age : 76,
+ }
+
+ console.log(typeof person); //object

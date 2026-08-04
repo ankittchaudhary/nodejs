c5d42b97a1edf6f2bf06caa1b01b5fae9641fccd
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-08-04 22:46:34 +0530

    feat: arrays properties and methods add

--- 01-JS-Basics/10-Arrays.js
+++ 01-JS-Basics/10-Arrays.js
@@ -22,4 +22,48 @@ console.log(typeof vegetables); //object
 console.log(fruits[0]); // returns very first elemenet //Apple
 console.log(fruits[1]); // returns second element //Banana .. and so on
 
+//=========ARRAY PROPERTIES=========
+
+//1. Length
+console.log(fruits.length); //4 // returns the number of elements in the array
+//2. IndexOf
+console.log(fruits.indexOf("Mango")); //2 // returns the index of the element in the array, if not found returns -1
+//3. Includes
+console.log(fruits.includes("Banana")); // checks if the element is present in the array or not, returns true or false
+
+
+//=========ARRAY METHODS=========
+
+//1. Push
+fruits.push("Grapes"); // adds an element at the end of the array
+console.log(fruits); //["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+//2. Pop
+fruits.pop(); // removes the last element from the array
+console.log(fruits); //["Apple", "Banana", "Mango", "Orange"]
+
+//3. Shift
+fruits.shift(); // removes the first element from the array
+console.log(fruits); //["Banana", "Mango", "Orange"]
+
+//4. Unshift
+fruits.unshift("Strawberry"); // adds an element at the beginning of the array
+console.log(fruits); //["Strawberry", "Banana", "Mango", "Orange"]
+
+//5. Splice
+fruits.splice(1, 1); // removes 1 element from index 1
+console.log(fruits); //["Strawberry", "Mango", "Orange"]
+
+//6. Slice
+let newFruits = fruits.slice(1, 3); // returns a new array containing elements from index 1 to 2 (3 is not included)
+console.log(newFruits); //["Mango", "Orange"]
+
+//7. Concat
+let allFruits = fruits.concat(vegetables);
+console.log(allFruits); //["Strawberry", "Mango", "Orange", "Carrot", "Broccoli", "Spinach"]
+
+//8. Join
+let fruitsString = fruits.join(", ");
+console.log(fruitsString); // returns a string of all elements in the array separated by a comma and space //Strawberry, Mango, Orange
+
 

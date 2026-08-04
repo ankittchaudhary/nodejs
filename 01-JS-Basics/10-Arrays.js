55fd8d1290db2f48e583ac3bf12530b629949d26
Ankit Chaudhary <chaudharyankit2022@gmail.com>
2026-08-04 22:43:55 +0530

    docs: arrays introduction add

---
+++ 01-JS-Basics/10-Arrays.js
@@ -0,0 +1,25 @@
+//=========ARRAYS=========
+
+/*
+1. Array is a special variable, which can hold more than one value at a time.
+2. It is a data structure that can store a collection of values.
+*/
+
+//=========CREATING AN ARRAY=========
+
+//1. Using Array Literal
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits);
+
+//2. Using Array Constructor
+let vegetables = new Array("Carrot", "Broccoli", "Spinach");  //new is keyword used to create an instance of an object, istance is like blueprint of an object, an object is like a real world enitity.
+console.log(vegetables);
+
+console.log(typeof vegetables); //object
+
+//=========ACCESSING ARRAY ELEMENTS=========
+
+console.log(fruits[0]); // returns very first elemenet //Apple
+console.log(fruits[1]); // returns second element //Banana .. and so on
+
+
